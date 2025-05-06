e6c22249900acb9e8d249cda43c57bf4feed7bfd
alex <alexfraleda@gmail.com>
2025-05-06 10:24:25 +0200

    comit

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
 from flask import Flask , render_template
+from datetime import date
+
+app = Flask(__name__)
+
+@app.route('/dashboard')
+def tienda():
+
+    nombre_admin = "Francisco"
+    tienda = "TecnoMarket"
+    fecha = date.today()
+
+
+    producto = {
+        "nombre": "ratón inalámbrico",
+        "descripcion": "Dispositivo ergonómico con conexión Bluetooth",
+        "precio": 19.99
+    }
+    return render_template("producto.html", producto=producto)
+
+if __name__ == '__main__':
+    app.run()
