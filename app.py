4bdbdd9c125310daafac87d89452f3bdee470b31
alex <alexfraleda@gmail.com>
2025-05-12 11:40:14 +0200

    comit

--- app.py
+++ app.py
@@ -1,22 +1,25 @@
-from flask import Flask , render_template , request
+from flask import Flask , render_template , request , redirect, url_for
 from datetime import date
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
-@app.route('/dashboard')
-def tienda():
+online = MongoClient("mongodb+srv://arodsan013a:T1L7WBu0UDrxvMtH@alexz.lqhssf7.mongodb.net/")
 
-    nombre_admin = "Francisco"
-    tienda = "TecnoMarket"
-    fecha = date.today()
+app.db = online.tienda
 
+nombre_admin = "Francisco"
+tienda = "TecnoMarket"
+fecha = date.today()
 
-    productos = [
-        {"nombre": "Placa Asus", "precio": 300.0, "stock": 2, "categoria": "Componentes"},
-        {"nombre": "Raton Stellseries", "precio": 15.0, "stock": 5, "categoria": "Accesorios"},
-        {"nombre": "Monitor Asus", "precio": 249.99, "stock": 0, "categoria": "Pantallas"},
-        {"nombre": "Teclado Ozono", "precio": 65.0, "stock": 20, "categoria": "Accesorios"}
-    ]
+@app.route('/')
+def inicio():
+    pagina = "indice"
+
+    
+
+    productos = [producto for producto in app.db.productos.find({})]
+
     clientes = [
         {"nombre": "Alejandro Rodriguez", "email": "alexfraleda@correo.com", "activo": True, "pedidos": 10},
         {"nombre": "Pepe Sanchez", "email": "pepe@correo.com", "activo": False, "pedidos": 1},
@@ -28,17 +31,7 @@ def tienda():
         {"cliente": "Manolo Gomez", "total": 5000.0, "fecha": "2025-05-04"}
     ]
 
-    prod={}
-    pedid = {}
-    if request.method == "POST":
-        prod = {
-            "nombre": request.form.get("product"),
-            "precio": float(request.form.get("price")),
-            "stock": int(request.form.get("stock")),
-            "categoria": request.form.get("category")
-            }
-        productos.append(prod)
-
+    
     total_stock = 0
     for producto in productos:
         total_stock += producto["stock"]
@@ -60,7 +53,53 @@ def tienda():
         total += pedido["total"]
 
 
-    return render_template("dashboard.html",nombre_admin=nombre_admin,tienda=tienda,fecha=fecha,productos=productos,clientes=clientes,pedidos=pedidos,total_stock=total_stock,clientes_activos=clientes_activos,cliente_pedido=cliente_pedido,total=total)
+    return render_template("dashboard.html",nombre_admin=nombre_admin,tienda=tienda,fecha=fecha,productos=productos,clientes=clientes,pedidos=pedidos,clientes_activos=clientes_activos,cliente_pedido=cliente_pedido,total=total, pagina=pagina ,total_stock=total_stock)
 
+@app.route("/productos", methods=["GET", "POST"])
+def productos():
+    productos = [producto for producto in app.db.productos.find({})]
+
+    pagina = "productos"
+    
+    if request.method == "POST":
+        formulario = request.form.get("tipo_formulario")
+        if formulario == "agregar_productos":
+            prod = {
+                "nombre": request.form.get("product"),
+                "precio": float(request.form.get("price")),
+                "stock": int(request.form.get("stock")),
+                "categoria": request.form.get("category")
+            }
+            app.db.productos.insert_one(prod)
+            return redirect(url_for("productos"))
+        
+        elif formulario == "eliminar_productos":
+            eliprod = request.form.get("elproduct")
+            app.db.productos.delete_one({"nombre": eliprod})
+            return redirect(url_for("productos"))
+        
+        elif formulario == "cambiar_stock":
+            nombre_prod = request.form.get("prod_a_cambiar").strip()
+            nuevo_stock = request.form.get("nuevo_stock")
+            nuevo_precio = request.form.get("nuevo_precio")
+
+            cambios = {}
+            if nuevo_stock:
+                cambios["stock"] = int(nuevo_stock)
+            if nuevo_precio:
+                cambios["precio"] = float(nuevo_precio)
+
+            if cambios:
+                app.db.productos.update_one({"nombre": nombre_prod}, {"$set": cambios})
+            
+            return redirect(url_for("productos"))
+
+
+    return render_template("dashboard.html", 
+                           nombre_admin=nombre_admin, 
+                           tienda=tienda, 
+                           fecha=fecha, 
+                           productos=productos,pagina=pagina,
+                           )
 if __name__ == '__main__':
     app.run()
